c3ed2920075b82cdc538d601ea9670d028022c43
ronia-jakim <weronikajakimowicz2@gmail.com>
2022-10-12 11:51:29 +0200

    analiza-fun-na-wykladzie-alg

--- anal_num/listy-zadan/01_rozw/01_zad7.jl
+++ anal_num/listy-zadan/01_rozw/01_zad7.jl
@@ -2,11 +2,17 @@ using Base
 
 # np 0.6833160899458118
 
-for i in 1:2^20
-    x = rand(Float64)
-    if (x * (1 / x)) != 1
-        println(i)
-        println(x)
-        break
+function fl()
+    x = one(Float64)
+
+    while x < 2.0
+        if x * (1/ x) != 1
+            println(x)
+            println(x * (1 / x))
+            break
+        end
+        x += eps(x)
     end
 end
+
+fl()
