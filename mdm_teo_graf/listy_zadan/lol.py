d8ad862a792919a3ad37da7b042658fb97050685
maruda <weronikajakimowicz2@gmail.com>
2022-10-25 14:08:10 +0200

    nudny-mdm

--- mdm_teo_graf/listy_zadan/lol.py
+++ mdm_teo_graf/listy_zadan/lol.py
@@ -1,15 +1,11 @@
-fuck = [1]
+def test(prev, prevprev, n):
+    now = 1 + prev
+    now /= prevprev
+    if now == 0 and n < 300:
+        print("OSZ KURWA ", n)
+    elif n < 300:
+        test(now, prev, n+1)
 
-def count(n, prev, prevprev):
-    now = fuck[n-2]
-    now *= prev * prevprev
-    now /= n
 
-    pom = fuck[n-2]*(n-1)
-    fuck.append(pom)
 
-    print(now, " | ", n)
-    if n+1 < 10:
-        count(n+1, now, prev)    
-
-count(2, 2, 1)
+test(1, -2.0000001, 2)
