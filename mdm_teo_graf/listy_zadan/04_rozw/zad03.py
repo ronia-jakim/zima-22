669d176efc67555bb3086649321984b31e0b63d0
ronia-jakim <weronikajakimowicz2@gmail.com>
2022-11-14 23:40:59 +0100

    mam-dosc

--- mdm_teo_graf/listy_zadan/04_rozw/zad03.py
+++ mdm_teo_graf/listy_zadan/04_rozw/zad03.py
@@ -13,14 +13,60 @@ def ext_gcd(a, b):
         S = [S[1], S[0] - q * S[1]]
         T = [T[1], T[0] - q * T[1]]
 
-    print(S[0], T[0])
+    ret = [S[0], T[0]]
 
-    return R[0]
-
-"""
-        ooooooo
-       ooo oooo
-    o oo    oo oo
-"""
+    return ret
 
 def zad(k, M):
+    if k == 1: return [M[0]]
+    if k == 2:
+        return ext_gcd(M[0], M[1])
+
+    else:
+        L = []
+        R = []
+
+        i = 0
+        
+        while i < k//2:
+            L.append(M[i])
+            i += 1
+        while i < k:
+            R.append(M[i])
+            i += 1
+
+        coef_L = zad(k//2, L)
+        coef_R = zad(k-k//2, R)
+
+        new_L = []
+        new_R = []
+
+        i = 0
+        while i < len(L): 
+            new_L.append(coef_L[i] * L[i])
+            i += 1
+
+        i = 0
+        while i < len(R): 
+            new_R.append(coef_R[i] * R[i])
+            i += 1
+
+        sum_L = sum(new_L)
+        sum_R = sum(new_R)
+
+        vec = ext_gcd(sum_L, sum_R)
+
+        i = 0
+        while i < len(L):
+            coef_L[i] *= vec[0]
+            i += 1
+        i = 0
+        while i < len(R):
+            coef_R[i] *= vec[1]
+            i += 1
+
+        ret = coef_L + coef_R
+
+        return ret
+
+print(zad(5, [3, 9, 21, 6, 27]))
