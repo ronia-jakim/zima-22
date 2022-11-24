26db1541be2b24a44dc8d352bc054b4bdd6fe700
ronia-jakim <weronikajakimowicz2@gmail.com>
2022-11-24 11:00:47 +0100

    chuj-w-dupe-numerkom-chce-spac

---
+++ anal_num/czy_ujebane.py
@@ -0,0 +1,17 @@
+MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5]
+MOJE = [7.5, 6, 8.5, 9, 7, 4]
+
+suma_max = sum(MAX)
+suma_moje = sum(MOJE)
+
+MOD1 = [7.5, 8.5, 11.5, 8.5]
+MOD1MY = [7.5, 6, 8.5, 9]
+
+MOD2 = [8, 10.5]
+MOD2MY = [7, 4]
+
+print(suma_moje / suma_max)
+
+print(sum(MOD1MY) / sum(MOD1))
+
+print(sum(MOD2MY) / sum(MOD2))
