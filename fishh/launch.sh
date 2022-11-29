a98f7897bb8d84ce7f9549917c1c7d79e1d369c6
guwno <weronikajakimowicz2@gmail.com>
2022-11-29 14:13:58 +0100

    anal_fun

--- fishh/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar --config=~/.config/polybar/fish/fish.ini
--- fishh/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $(iwgetid -r | wc -c) -eq 0 ];
-then
-    echo "%{F#585b70}%{F-}"
-else
-    echo "%{F#a6e3a1}%{F-}"
-fi
