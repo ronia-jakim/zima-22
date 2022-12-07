edc1c1ff3cd607d36ef826cb2a37bfde65d97add
maruda-i-chuj <weronikajakimowicz2@gmail.com>
2022-12-07 22:25:22 +0100

    elo-numer

--- anal_num/czy_ujebane.py
+++ anal_num/czy_ujebane.py
@@ -1,5 +1,5 @@
-MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5]
-MOJE = [7.5, 6, 8.5, 9, 7, 4]
+MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5, 10]
+MOJE = [7.5, 6, 8.5, 9, 7, 5, 6.5]
 
 suma_max = sum(MAX)
 suma_moje = sum(MOJE)
@@ -7,11 +7,17 @@ suma_moje = sum(MOJE)
 MOD1 = [7.5, 8.5, 11.5, 8.5]
 MOD1MY = [7.5, 6, 8.5, 9]
 
-MOD2 = [8, 10.5]
-MOD2MY = [7, 4]
+MOD2 = [8, 10.5, 10]
+MOD2MY = [7, 5, 6.5]
 
-print(suma_moje / suma_max)
+proc_1 = sum(MOD1MY) / sum(MOD1)
+proc_2 = sum(MOD2MY) / sum(MOD2)
 
-print(sum(MOD1MY) / sum(MOD1))
+if proc_1 < 0.4 or proc_2 < 0.4:
+    print("TAK DEBILU")
+else:
+    print(suma_moje / suma_max)
 
-print(sum(MOD2MY) / sum(MOD2))
+    print(proc_1)
+
+    print(proc_2)
