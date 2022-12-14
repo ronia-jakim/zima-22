52b3ec59daf5d9243f4b1b86fdee516c1a4a77b9
ronia-jakim <weronikajakimowicz2@gmail.com>
2022-12-14 18:14:26 +0100

    nienawidze-numerkow

--- anal_num/czy_ujebane.py
+++ anal_num/czy_ujebane.py
@@ -1,5 +1,5 @@
-MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5, 10]
-MOJE = [7.5, 6, 8.5, 9, 7, 5, 6.5]
+MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5, 10, 11.5]
+MOJE = [7.5, 6, 8.5, 9, 7, 5, 6.5, 0]
 
 suma_max = sum(MAX)
 suma_moje = sum(MOJE)
@@ -7,8 +7,8 @@ suma_moje = sum(MOJE)
 MOD1 = [7.5, 8.5, 11.5, 8.5]
 MOD1MY = [7.5, 6, 8.5, 9]
 
-MOD2 = [8, 10.5, 10]
-MOD2MY = [7, 5, 6.5]
+MOD2 = [8, 10.5, 10, 11.5]
+MOD2MY = [7, 5, 6.5, 0]
 
 proc_1 = sum(MOD1MY) / sum(MOD1)
 proc_2 = sum(MOD2MY) / sum(MOD2)
