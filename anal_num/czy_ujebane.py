70880b7699d281841bce436b3f04d083c6658c75
chuj-w-dupie <weronikajakimowicz2@gmail.com>
2023-01-03 23:44:07 +0100

    numerki

--- anal_num/czy_ujebane.py
+++ anal_num/czy_ujebane.py
@@ -7,8 +7,8 @@ suma_moje = sum(MOJE)
 MOD1 = [7.5, 8.5, 11.5, 8.5]
 MOD1MY = [7.5, 6, 8.5, 9]
 
-MOD2 = [8, 10.5, 10, 11.5]
-MOD2MY = [7, 5, 6.5, 0]
+MOD2 = [8, 10.5, 10, 11.5, 11]
+MOD2MY = [7, 5, 6.5, 4, 0]
 
 proc_1 = sum(MOD1MY) / sum(MOD1)
 proc_2 = sum(MOD2MY) / sum(MOD2)
