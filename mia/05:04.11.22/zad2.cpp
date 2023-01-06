bf28d155607b8089f1efee59e248e0a4e8bf3702
ronia-jakim <weronikajakimowicz2@gmail.com>
2023-01-06 19:43:56 +0100

    mic

--- mia/05:04.11.22/zad2.cpp
+++ mia/05:04.11.22/zad2.cpp
@@ -5,18 +5,22 @@
 
 using namespace std;
 
+#define ll long long
+
 int n;
 vector<int> somsiad[150000];
 int odwiedzone[150000];
 
-bool dfs(int tato, int dziadzio);
+ll krawedzie, wierzcholki;
+
+void dfsik(int v);
 
 int main () {
     int n,m;
 
     cin >> n >> m;
 
-    for (int i = 0; i < 150000; i++) odwiedzone[i] = 0;
+    for (int i = 0; i < n; i++) odwiedzone[i] = 0;
 
     for (int i = 0; i < m; i++) {
         int a, b;
@@ -26,11 +30,34 @@ int main () {
         somsiad[b].push_back(a);
     }
 
+    for (int i = 0; i < n; i++) {
+        if (odwiedzone[i] == 0) {
+            krawedzie = 0;
+            wierzcholki = 0;
+            dfsik(i);
+
+            ll klikowo = wierzcholki * (wierzcholki - 1);
+            if (klikowo != krawedzie) {
+                cout << "NO" << endl;
+                return 0; 
+            }
+        }
+    }
+
     cout << "YES" << endl;
 
     return 0;
 }
 
-bool dfs(int tato, int dziadzio) {
-    
+void dfsik(int v) {
+    //if (odwiedzone[v]) return;
+
+    odwiedzone[v] = 2137;
+    wierzcholki++;
+    krawedzie += somsiad[v].size();
+
+    for (int i = 0; i < somsiad[v].size(); i++) {
+        int next = somsiad[v][i];
+        if (odwiedzone[next] == 0) dfsik(next);
+    }
 }
