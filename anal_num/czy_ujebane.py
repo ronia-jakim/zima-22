ce150c0082cf40a372029cd086215b54e9b89f17
chuj-w-dupie <weronikajakimowicz2@gmail.com>
2023-01-11 20:36:41 +0100

    numerkiii

--- anal_num/czy_ujebane.py
+++ anal_num/czy_ujebane.py
@@ -1,8 +1,5 @@
-MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5, 10, 11.5]
-MOJE = [7.5, 6, 8.5, 9, 7, 5, 6.5, 4]
-
-suma_max = sum(MAX)
-suma_moje = sum(MOJE)
+#MAX = [7.5, 8.5, 11.5, 8.5, 8, 10.5, 10, 11.5]
+#MOJE = [7.5, 6, 8.5, 9, 7, 5, 6.5, 4]
 
 MOD1 = [7.5, 8.5, 11.5, 8.5]
 MOD1MY = [7.5, 6, 8.5, 9]
@@ -10,14 +7,28 @@ MOD1MY = [7.5, 6, 8.5, 9]
 MOD2 = [8, 10.5, 10, 11.5, 11]
 MOD2MY = [7, 5, 6.5, 4, 2]
 
+MOD3 = [15.5]
+MOD3MY = [8.5]
+
+suma_max = sum(MOD1) + sum(MOD2) + sum(MOD3)
+suma_moje = sum(MOD1MY) + sum(MOD2MY) + sum(MOD3MY)
+
 proc_1 = sum(MOD1MY) / sum(MOD1)
 proc_2 = sum(MOD2MY) / sum(MOD2)
+proc_3 = sum(MOD3MY) / sum(MOD3)
 
-if proc_1 < 0.4 or proc_2 < 0.4:
+if proc_1 < 0.4 or proc_2 < 0.4 or proc_3 < 0.4:
     print("TAK DEBILU")
+    print(proc_1)
+
+    print(proc_2)
+
+    print(proc_3)
 else:
     print(suma_moje / suma_max)
 
     print(proc_1)
 
     print(proc_2)
+
+    print(proc_3)
