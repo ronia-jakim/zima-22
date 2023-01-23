6792e772347cb06e6dcfcd03100c8a79ad0da26c
ronia-jakim <weronikajakimowicz2@gmail.com>
2023-01-23 17:54:30 +0100

    nie-wiem

--- mia/10:09.12.22/zad1.cpp
+++ mia/10:09.12.22/zad1.cpp
@@ -11,6 +11,7 @@ ll n, d;
 
 vector<ll> points;
 vector<ll> distances[100001];
+vector<ll> max_dst;
 
 int main () {
     cin >> n >> d;
@@ -19,19 +20,29 @@ int main () {
         ll pom;
         cin >> pom;
         points.push_back(pom);
-        //if (i>0) points[i] += points[i-1];
-        if(i == 0) distances[i].push_back(0);
-        for(ll j = 0; j <= i; j++) {
-            distances[i].push_back(pom - points[j]);
+
+        max_dst.push_back(0);
+        for (ll j = i-1; j >= 0; j--) {
+            ll dst = pom - points[j];
+            if (dst <= d && max_dst[j] <= dst) max_dst[j] = j;
         }
+
+        // else {
+        //     for(ll j = 0; j <= i; j++) {
+        //         distances[j].push_back(pom - points[j]);
+        //     }
+        // }
     }
 
     ll ret = 0;
 
-    for (ll i = 0; i < n; i++) {
-        sort(distances[i].begin(), distances[i].end());
-        for(ll j = 0; j < distances[i].length())
+    for (ll j = 0; j < n-1; j++) {
+        if (max_dst[j] >= max_dst[j+1]) {
+            ret += 
+        }
     }
+
+    cout << ret << endl;
 
     return 0;
 }
